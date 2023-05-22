4765c43c4ba5062aa961e8ae4026efa3512f53ef
Juma <juma.musa@student.moringaschool.com>
2023-05-22 12:51:14 +0300

    Done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,24 +10,25 @@ class CashRegister
   
     def add_item(title, price, quantity = 1)
       quantity.times do
-        self.items << title
+        self.items << title # Adds the item's title to the items array for each quantity
       end
-      self.total += price * quantity
-      self.last_transaction_amount = price * quantity
+      self.total += price * quantity # Calculates the total price by multiplying the price with the quantity
+      self.last_transaction_amount = price * quantity # Updates the last_transaction_amount to the price multiplied by the quantity
     end
   
     def apply_discount
       if discount > 0
-        discount_amount = (total * (discount.to_f / 100)).to_i
-        self.total -= discount_amount
-        "After the discount, the total comes to $#{total}."
+        discount_amount = (total * (discount.to_f / 100)).to_i # Calculates the discount amount as a percentage of the total price
+        self.total -= discount_amount # Subtracts the discount amount from the total
+        "After the discount, the total comes to $#{total}." # Returns a message indicating the new total after applying the discount
       else
-        "There is no discount to apply."
+        "There is no discount to apply." # Returns a message indicating that there is no discount to apply
       end
     end
   
     def void_last_transaction
-      self.total -= last_transaction_amount
+      self.total -= last_transaction_amount # Subtracts the last transaction amount from the total to void the last transaction
     end
   end
+  
   
